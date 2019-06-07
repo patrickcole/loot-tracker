5b8bc0575459f137049d5b67aaae728bcb27ab5e
Patrick Cole <hpcmobiledev@gmail.com>
2019-06-07 11:36:47 -0400

    Adds react-router to front-end to handle changes
    * Loot items now display on the main page
    * Each item has dedicated route with listings and item info
    * Locations are listed with coordinates

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 const getDataAsync = async function (url) {
@@ -13,7 +15,9 @@ function ItemsList( { data } ) {
     <ul>
       {
         data.map( item => {
-          return <li>{ item.title }</li>
+          return (
+            <li><Link to={`/item/${item.slug}`}>{ item.title }</Link></li>
+          )
         })
       }
     </ul>
@@ -37,27 +41,93 @@ function LocationsList( { data } ) {
   )
 }
 
-function App() {
+function Listings({ data }) {
+  return (
+    <ul>
+    { data.map((listing) => {
+      return <li>{ listing.price } - <Link to={`/location/${listing.slug}`}>{ listing.slug }</Link> at { listing.date }</li>
+    })}
+    </ul>
+  )
+}
 
-  const [locations, setLocations] = useState([]);
+function Item({ location }) {
+
+  let default_item = { title: "", type: { category: "", system: "" }, locations: [] }
+
+  let [item, setItem] = useState(default_item);
+
+  useEffect(
+    () => {
+      let slug = location.pathname;
+      slug = slug.replace('/item/','');
+      getDataAsync(`/api/item/${slug}`).then( (response) => {
+        setItem(response.data)
+      })
+    }, [ location ]
+  );
+  
+
+  return (
+    <main>
+      <h3>{ item.title }</h3>
+      <p>{ item.type.category }</p>
+      <p>{ item.type.system }</p>
+      <Listings data={item.locations} />
+    </main>
+  );
+}
+
+function Location( { location } ) {
+
+  const [place, setPlace] = useState({ title: "", coordinates: "" });
+
+  useEffect(
+    () => {
+      let slug = location.pathname;
+      slug = slug.replace('/location/','');
+      getDataAsync(`/api/location/${slug}`).then( (response) => {
+        setPlace(response.data)
+      })
+    }, [ location ]
+  );
+
+  return (
+    <main>
+      <h3>{place.title}</h3>
+      <p>{place.coordinates}</p>
+    </main>
+  )
+}
+
+function Main() {
+
   const [items, setItems] = useState([]);
 
   useEffect(
     () => {
-      getDataAsync(`/api/locations`).then( response => setLocations(response.data) );
       getDataAsync(`/api/items`).then( response => setItems(response.data) );
     }, []
   );
 
   return (
-    <div className="App">
-      <h1>Loot Tracker</h1>
-      <hr />
+    <main>
       <h2>Items</h2>
       <ItemsList data={items} />
-      <h2>Locations</h2>
-      <LocationsList data={locations} />
-    </div>
+    </main>
+  )
+}
+
+function App() {
+
+  return (
+    <Router>
+      <h1>Loot Tracker</h1>
+      <hr />
+      <Route path="/" exact render={ props => <Main { ...props } /> } />
+      <Route path="/item/:slug" exact render={ props => <Item {...props} /> } />
+      <Route path="/location/:slug" exact render={ props => <Location {...props} /> } />
+    </Router>
   );
 }
 
